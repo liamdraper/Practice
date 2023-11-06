4855c7449bbacb200b6e88b0201e8fcfaa3a28e8
liamdraper <liamdraper83@gmail.com>
2023-11-05 18:08:28 -0600

    To-do app

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
+
+const todos = [];
+
 // HTML elements
-const home = document.querySelector('.link1');
-const about = document.querySelector('.link2');
-const projects = document.querySelector('.link3');
-const contact = document.querySelector('.link4');
 
 const btn = document.querySelector('button');
+const ul = document.querySelector('ul');
+const text = document.querySelector('textarea');
 
-// State variables
-let isMenuOpen = false;
+// Event Listeners
 
-// Event listeners
-
-btn.addEventListener('click', () => {
-    if (!isMenuOpen) {
-        openMenu();
-    }
-    else {
-        closeMenu();
-    }
-    isMenuOpen = !isMenuOpen;
+btn.addEventListener('click', ()=> {
+    addTodo();
 })
 
 // Functions
 
-function closeMenu() {
-    home.style.display = 'none';
-    about.style.display = 'none';
-    projects.style.display = 'none';
-    contact.style.display = 'none';
+function addTodo() {
+    // need to:  create element to hold
+    const todo = document.createElement('li');
+    todo.innerText = text.value;
+    const editBtn = document.createElement('button');
+    editBtn.innerText = 'Edit';
+    editBtn.addEventListener('click', editTodo);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.addEventListener('click', deleteTodo);
+    ul.appendChild(todo);
+    todo.appendChild(editBtn);
+    todo.appendChild(deleteBtn);
+    text.value = '';
+    console.log(todo);
 }
 
-function openMenu() {
-    home.style.display = '';
-    about.style.display = '';
-    projects.style.display = '';
-    contact.style.display = '';
+function editTodo(e) {
+    const editTextArea = document.createElement('textarea');
+    editTextArea.innerText = e.target.parentNode.innerText;
+    e.target.parentNode.appendChild(editTextArea);
+    const closeEditBtn = document.createElement('button');
+    closeEditBtn.innerText = 'X';
+    const saveEdit = document.createElement('button');
+    saveEdit.innerText = 'Save';
+    editTextArea.appendChild(closeEditBtn, saveEdit);
+    console.log(editTextArea, e.target.parentNode);
+    // need to wrap the text area and 2 buttons inside a div
+
 }
 
+function deleteTodo(e) {
+    ul.removeChild(e.target.parentNode);
+}
 
-// Initializes menu as closed
-closeMenu();
